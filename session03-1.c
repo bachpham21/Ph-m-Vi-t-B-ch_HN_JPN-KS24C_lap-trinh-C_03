3c68d823e8049ac328852033c21b6a8333f3dea7
bachpham21 <bachpvk8a@gmail.com>
2024-11-26 11:07:29 +0700

    3-1

---
+++ session03-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    char ten[50]; 
+
+    printf("Vui long nhap ten cua ban: "); 
+    scanf("%49s", ten); 
+
+    printf("Xin chao %s!\n", ten); 
+
+    return 0;
+}
